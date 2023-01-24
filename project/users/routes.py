96f053f499b6c3508bbad61cd629b5386653876f
PhilipJPrice <philip.price.1994@gmail.com>
2023-01-23 20:30:35 -0800

    Error Pages

--- project/users/routes.py
+++ project/users/routes.py
@@ -10,3 +10,13 @@ from flask import render_template, flash
 def about():
         flash('Thanks for learning about this site!', 'info')
         return render_template('users/about.html', company_name='TestDriven.io')
+
+@users_blueprint.errorhandler(403)
+def page_forbidden(e):
+    return render_template('users/403.html'), 403
+
+from flask import abort
+
+@users_blueprint.route('/admin')
+def admin():
+    abort(403)
